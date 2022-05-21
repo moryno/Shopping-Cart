3e84cedec5876ad3a1c5cc921972229bb143cf7b
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-05-21 14:28:45 +0300

    Add a navigation section in the shopping cart website

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+// fetch("./assets/js/data.json")
+//   .then(response => response.json())
+//   .then(data => appendData(data))
+//   .catch(err => console.log(err))
+
+// const appendData = (data) => {
+//     const mainContainer = document.getElementById("cartContainer")
+//     data.forEach((item) => {
+//         const cartEvent = document.createElement("div")
+//         cartEvent.innerHTML = "Title: " + item.title;
+//         mainContainer.append(cartEvent)
+//     })
+   
+// }
