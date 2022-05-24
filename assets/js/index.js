4a2cecb61050aa1de7a48049d385670b45b3c4de
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-05-24 09:15:57 +0300

    Add a carousel to the tilte slides of the landing page

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,3 +12,38 @@
 //     })
    
 // }
+
+// ADD CAROUSEL TO THE SLIDER ITEMS
+const sliderContainer = document.querySelector(".sliderWrapper");
+const singleSlides = Array.from(sliderContainer.children);
+const nextButton = document.querySelector(".arrowRight");
+const prevButton = document.querySelector(".arrowLeft");
+const slideWidth = singleSlides[0].getBoundingClientRect().width;
+
+// 1. Arrange slide next to one another
+const setSlidePosition = (singleSlide, i) => {
+    singleSlide.style.left = slideWidth * i + "px"      
+}
+singleSlides.forEach(setSlidePosition)
+
+// 2. A function to move slides to the left or right
+const sliderMove = (sliderContainer, currentSlide, targetSlide) => {
+    sliderContainer.style.transform = `translateX(-${targetSlide.style.left})`
+    currentSlide.classList.remove("currentSlide");
+    targetSlide.classList.add("currentSlide");
+}
+
+// move right
+nextButton.addEventListener("click", function(){
+    const currentSlide = sliderContainer.querySelector(".currentSlide");
+    const nextSlide = currentSlide.nextElementSibling;
+    
+    sliderMove(sliderContainer, currentSlide, nextSlide)
+})
+
+// // move left
+prevButton.addEventListener("click", function(){
+    const currentSlide = sliderContainer.querySelector(".currentSlide");
+    const prevSlide = currentSlide.previousElementSibling;
+    sliderMove(sliderContainer, currentSlide, prevSlide)
+})
